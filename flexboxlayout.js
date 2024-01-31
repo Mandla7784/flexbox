5c1a6fa7107f410ccdf03f032e9f5b64554df2f9
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-01-31 21:52:33 +0200

    Add files via upload

---
+++ flexboxlayout.js
@@ -0,0 +1,11 @@
+//cart variables
+
+const btnBuy = document.querySelector('#btn-buy');
+const itemInut = document.getElementById('item');
+
+
+
+btnBuy.addEventListener('click', function(){
+    let inpuValu = itemInut.value;
+      itemInut.value = "";
+})
